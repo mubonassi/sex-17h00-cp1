3ad31d3666a2bf7cd38afe3e27656b18e55f316f
mubonassi <115490684+mubonassi@users.noreply.github.com>
2026-08-14 21:23:46 +0000

    asfsgdfbfghdfgsdfs

--- 2 - Analise de Dados/conteudos/conteudo3.py
+++ 2 - Analise de Dados/conteudos/conteudo3.py
@@ -15,9 +15,34 @@ tabela = pd.DataFrame(dados)
 #Exibindo a tabela de forma completa
 print(tabela)
 print("-"*60)
+
+#Consultando dados
 #Exibindo uma coluna
 print(tabela["Nome"])
 print("-"*60)
+
 #Exibindo uma linha especifica
 print(tabela.loc[0])
 print("-"*60)
+
+#Exibindo uma célula específica
+#Linha 0, coluna "Nome"
+#loc[x,y] -> X: a linha; Y: a coluna
+print(tabela.loc[2,"Nome"])
+
+#Alterar e adicionar valores
+#Alterando
+tabela.loc[3,"Nome"] = "Jorisvaldo"
+
+#Adicionar uma nova linha
+tabela.loc[4] = ["Roberto Carlos",87]
+
+#Adicionando na PRÓXIMA linha disponível
+#Utilizando a função len()
+tabela.loc[len(tabela)] = ["Próximo Nome",300]
+
+#Deletando uma linha da tabela
+#Utilizando drop()
+tabela = tabela.drop(2)
+
+print(tabela)
